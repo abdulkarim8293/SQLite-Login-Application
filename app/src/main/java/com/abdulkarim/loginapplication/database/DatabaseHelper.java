7af4e9f77f444516465a10430e8c6bcdda88df5c
unknown <abdulkarim8293@gmail.com>
2020-04-13 23:41:33 +0600

    update database helper class

--- app/src/main/java/com/abdulkarim/loginapplication/database/DatabaseHelper.java
+++ app/src/main/java/com/abdulkarim/loginapplication/database/DatabaseHelper.java
@@ -1,20 +1,23 @@
 package com.abdulkarim.loginapplication.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.abdulkarim.loginapplication.model.User;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private final static String DATABASE_NAME = "users.db";
     private static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_USER = "user_info";
+    private static final String TABLE_USER = "user_info";
 
-    public static final String USER_ID = "user_id";
-    public static final String USER_NAME = "user_name";
-    public static final String USER_EMAIL = "user_email";
-    public static final String USER_PASSWORD = "user_password";
+    private static final String USER_ID = "user_id";
+    private static final String USER_NAME = "user_name";
+    private static final String USER_EMAIL = "user_email";
+    private static final String USER_PASSWORD = "user_password";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -37,6 +40,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         db.execSQL(DROP_USER_TABLE);
         onCreate(db);
+    }
 
+    public void addUser(User user) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(USER_NAME, user.getName());
+        values.put(USER_EMAIL, user.getEmail());
+        values.put(USER_PASSWORD, user.getPassword());
+
+        db.insert(TABLE_USER, null, values);
+        db.close();
     }
 }
